10dc67ca31c8d380b276beea10508354c67035c4
srihari198 <srihari198@gmail.com>
2019-03-08 12:03:22 +0530

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conans import ConanFile, CMake
 class Pkg(ConanFile):
     name = "TestC"
-    version = "1.0.0"
+    version = "1.0.1"
     settings = "os", "compiler", "arch", "build_type"
     requires = None
     generators = "cmake"
