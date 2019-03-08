b90d6ab7872efa6ab61f9460df6997f5811953b2
srihari198 <srihari198@gmail.com>
2019-03-08 14:45:38 +0530

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ class Pkg(ConanFile):
     name = "TestC"
     version = "1.0.1"
     settings = "os", "compiler", "arch", "build_type"
-    requires = "plibsys/0.0.4@saprykin/stable --build missing"
+    requires = "plibsys/0.0.4@saprykin/stable"
     generators = "cmake"
     exports_sources = "src/*"
 
